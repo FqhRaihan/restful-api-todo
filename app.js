987f26ac71de41287342e7b2f0d1219ade2798f3
FqhRaihan <223210007@student.unaki.ac.id>
2023-11-14 17:13:52 +0700

    install packege express and create App.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+const app = express()
+
+const rootRoutes = require("./routes")
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(rootRoutes)
+
+
+app.listen(PORT, () => {
+    console.log("server running on port :"+ PORT)
+})
