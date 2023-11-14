4222a3e62e362d24177dba07c5aa658e2d6ac566
FqhRaihan <223210007@student.unaki.ac.id>
2023-11-14 22:28:39 +0700

    add config

--- models/Todos.js
+++ models/Todos.js
@@ -1,7 +1,24 @@
-let Todos = [
-    {id: 1, value: "belajar express"},
-    {id: 2, value: "belajar buat server"},
-    {id: 3, value: "belajar buat API"}
-]
-
-module.exports = Todos
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Todo extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Todo.init({
+    value: DataTypes.STRING,
+    status: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'Todo',
+  });
+  return Todo;
+};
